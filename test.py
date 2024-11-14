3151f5efd38b1254ef83521ae27bd60609c1648d
kaihe xu <xukaihenupt@gmail.com>
2024-11-14 20:48:01 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# Plotting the data
+plt.plot(x, y)
+
+# Adding labels and title
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Simple Line Graph')
+
+# Displaying the plot
+plt.show()
